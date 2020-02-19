ddce87739f6f9f50807e0d0722716909ff081c6e
Carlo Win Marshal <36812643+carwima@users.noreply.github.com>
2020-02-19 12:07:22 +0700

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
   //Set the response HTTP header with HTTP status and Content type
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hi World\n');
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
